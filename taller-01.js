c6209bf1f5b1a40aca89c422af7c013d5170870f
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-08-09 18:23:48 -0500

    Punto 1 Terminado

---
+++ taller-01.js
@@ -0,0 +1,9 @@
+// P1.Desarrolle una función llamada convertidorTemp que reciba
+// una temperatura en grados Celsius y la convierta a grados Fahrenheit.
+
+function convertidorTemp(celsius) {
+    return celsius * 9 / 5 + 32;
+}
+
+console.log(convertidorTemp(0)); // 32
+
