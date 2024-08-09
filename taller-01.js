06ecbf524c2356c37e9b90f3a0a02a62aeb36f97
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-08-09 18:25:42 -0500

    Punto 2 Terminado

--- taller-01.js
+++ taller-01.js
@@ -7,3 +7,15 @@ function convertidorTemp(celsius) {
 
 console.log(convertidorTemp(0)); // 32
 
+// P2. Desarrolle una función llamada resolvedor que retorne
+// el valor de x de la formula general de una ecuación cuadrática.
+// La función debe recibir tres parámetros: a, b y c. Además de un cuarto
+// parámetro opcional llamado signo que puede tener dos valores: + o -.
+// Si el signo es + se debe retornar el valor positivo de x y si es - se debe
+// retornar el valor negativo de x.
+
+function resolvedor(a, b, c, signo) {
+    let x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    let x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    return signo === '+' ? x1 : x2;
+}
