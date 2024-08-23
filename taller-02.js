6ea01acc3e7d913dedede76d1460ba3d9353d2ee
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2024-08-23 18:43:53 -0500

    Second point of Taller 2 finished

--- taller-02.js
+++ taller-02.js
@@ -2,10 +2,25 @@
 // Desarrolle una función llamada findMax que reciba una lista
 // de números por parametro y devuelva el número mayor de la lista.
 // no es permitido utilizar el método Math.max
-// Hint: Utilice el método reduce
+// Hint: Utilizar el método reduce
 
 function findMax(numbers) {
   return numbers.reduce((acc, curr) => (acc > curr ? acc : curr));
 }
 
-console.log(findMax([5, 4, 7, 6, 6, 1, 7]));
+// Punto 2
+//Desarrolle una función llamada includes que reciba una lista de números
+// y un número por parametro y devuelva true si el número se encuentra en la lista
+// o false en caso contrario.
+// no es permitido utilizar el método includes
+// Hint: Usar for each
+
+function includes(numbers, number) {
+  let result = false;
+  numbers.forEach((element) => {
+    if (element === number) {
+      result = true;
+    }
+  });
+  return result;
+}
